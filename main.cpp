0fb64d9fc07e796fe2cec7d22927effb45d8d428
yuki-inaho <yoshikawa@inaho.co>
2022-03-31 19:53:33 +0900

    Modify some enum values to be compatible with opencv4

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "opencv2/opencv.hpp"
 
 int main() {
-  cv::Mat image = cv::imread("1.png", CV_LOAD_IMAGE_GRAYSCALE);
+  cv::Mat image = cv::imread("1.png", cv::IMREAD_GRAYSCALE);
 
   Stag stag(15, 7, true);
 
